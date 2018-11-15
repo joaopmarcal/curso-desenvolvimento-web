0e532e08fdcfa336cdadab22c60a47dc54330782
Unknown <paulospc21@gmail.com>
2018-11-15 07:42:28 -0300

    estudo php

---
+++ php/mensagens_divertidas/mensagens_divertidas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+		<title>Mensagens divertidas</title>
+	</head>
+
+	<body>
+		<?php 
+			echo 'Teste de impressão';
+		?>
+		<br>
+		<?='teste tag impressão';?>
+		<? echo 'Teste Tag Curta'; ?>
+		<br>
+		<?php /*?>// tag descontinuada não funciona em php 7.<?php */?>
+		<% 
+		 echo 'Asp tag';
+		%>
+	</body>
+</html>
