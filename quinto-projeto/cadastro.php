3f492726e85c78bde8d2cf6ef75c7e90c77bc062
Unknown <paulospc21@gmail.com>
2018-11-17 15:16:14 -0300

    adiconado exemplo de swiitch

---
+++ quinto-projeto/cadastro.php
@@ -0,0 +1,33 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+</head>
+<?php
+	
+	if( isset($_POST['nome']) && empty($_POST['nome'])){
+		echo 'Preencha o nome completo <br>';
+	}
+	if( isset($_POST['cpf']) && empty($_POST['cpf'])){
+		echo 'Preencha o CPF <br>';
+	}
+	if( isset($_POST['cpf']) && !is_numeric($_POST['cpf'])){
+		echo 'Apenas números no CPF <br>';
+	}	
+	echo $data = date("Y-m-d H:i");
+?>	
+<body>
+<form action="" method="post">
+	<label for="">
+		Nome completo:
+		<input type="text" name="nome">
+	</label>
+	<label for="">
+		CPF:
+		<input type="text" name="cpf">
+	</label>	
+	<input type="submit" value="cadastrar">
+</form>
+</body>
+</html>
---
+++ quinto-projeto/switch.php
@@ -0,0 +1,32 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+</head>
+
+<body>
+<?php
+	$teste = '5';
+	
+	switch($teste){
+			case '1';
+				echo 'caso 1';
+				break;
+			case '2';
+				echo 'caso 2';
+				break;
+			case '3';
+				echo 'caso 3';
+				break;
+			default;
+				echo 'número incorreto';
+				break;
+			
+	}
+	
+?>	
+
+
+</body>
+</html>
