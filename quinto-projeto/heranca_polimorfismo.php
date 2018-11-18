08e8ee2961675f602f816460c923dd1e73e2c1ff
Unknown <paulospc21@gmail.com>
2018-11-18 13:39:59 -0300

    calculadora

---
+++ quinto-projeto/heranca_polimorfismo.php
@@ -0,0 +1,35 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+</head>
+<?php
+	//Classe mãe ou superclasse
+	class Felino{
+		var $mamifero = 'sim';
+		
+		function correr(){
+			echo 'Correr como felino';
+		}
+	}
+	
+	//Classe filha ou subclasse
+	class Chita extends Felino{
+		
+		//Polimorfismo
+		function correr(){
+			echo 'corre até 100km';
+		}
+		
+	}
+	class Gato extends Felino{
+		
+	}
+	$chita = new Chita();
+	echo $chita->mamifero . '<br>';
+	$chita->correr();
+?>	
+<body>
+</body>
+</html>
