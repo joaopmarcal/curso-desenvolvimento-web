83ea0f90f455fbdb8620be3c962d33298b035260
Unknown <paulospc21@gmail.com>
2018-11-22 11:10:08 -0200

    adicionado método post

---
+++ twitter_clone/registra_usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+	echo $_POST['usuario'];
+	echo $_POST['email'];
+	echo $_POST['senha'];
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
