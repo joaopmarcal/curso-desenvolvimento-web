fd4bb761ccfc04cc400b5d354889b10753555660
Unknown <paulospc21@gmail.com>
2018-11-24 11:46:51 -0300

    corrigido falha catastrófica no banco de dados

--- twitter_clone/registra_usuario.php
+++ twitter_clone/registra_usuario.php
@@ -6,12 +6,8 @@
 	$senha = $_POST['senha'];
 
 	$objDb = new db();
-	//$link = $objDb->conecta_mysql();
-	//$sql = " insert into usuarios(usuario,email,senha) values ('$usuario','$email','$senha') ";
-	$sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('" .$this->usuario. "', '" .$this->email."', '" .$this->senha."')";
-		
-	$con = Conexao::ligarConexao();
-	$con->exec($query);
+	$link = $objDb->conecta_mysql();
+	$sql = " insert into usuarios(usuario,email,senha) values ('$usuario','$email','$senha') ";
 	//executar a query
 	if(mysqli_query($link, $sql)){
 		echo 'Usuário registrado com sucesso!';
