7227d3a59900f716bb410f05a3a298033c25b5b2
João Paulo <paulospc21@gmail.com>
2018-11-28 09:17:51 -0300

    bloqueio de usuários já cadastrados

--- twitter_clone/registra_usuario.php
+++ twitter_clone/registra_usuario.php
@@ -7,6 +7,21 @@
 
 	$objDb = new db();
 	$link = $objDb->conecta_mysql();
+
+	//verifica se o usuário já existe
+	$sql = "select * from usuarios where usuario = '$usuario' ";
+	if($resultado_id = mysqli_query($link,$sql)){
+		
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+		if(isset($dados_usuario['usuario'])){
+			echo 'Usuário já cadastrado';
+		} else {
+			echo 'Erro ao tentar localizar o resgistro de usuário';
+		}
+	} else {
+		echo 'Erro ao tentar localizar o registro de usuário';
+	}
+	//verifica se o e-mail já existe
 	$sql = " insert into usuarios(usuario,email,senha) values ('$usuario','$email','$senha') ";
 	//executar a query
 	if(mysqli_query($link, $sql)){
