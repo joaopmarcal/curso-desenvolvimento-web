96cb01d5bdc77a3dde70beb4636487e8c650db4f
João Paulo <paulospc21@gmail.com>
2018-11-30 19:53:40 -0300

    bloqueio de cadastro de usuários repetidos

--- twitter_clone/registra_usuario.php
+++ twitter_clone/registra_usuario.php
@@ -8,20 +8,42 @@
 	$objDb = new db();
 	$link = $objDb->conecta_mysql();
 
+	$usuario_existe = false;
+	$email_existe = false;
+
 	//verifica se o usuário já existe
 	$sql = "select * from usuarios where usuario = '$usuario' ";
 	if($resultado_id = mysqli_query($link,$sql)){
 		
 		$dados_usuario = mysqli_fetch_array($resultado_id);
 		if(isset($dados_usuario['usuario'])){
-			echo 'Usuário já cadastrado';
-		} else {
-			echo 'Erro ao tentar localizar o resgistro de usuário';
-		}
+			$usuario_existe = true;
+		} 
 	} else {
 		echo 'Erro ao tentar localizar o registro de usuário';
 	}
 	//verifica se o e-mail já existe
+	$sql = "select * from usuarios where email = '$email' ";
+	if($resultado_id = mysqli_query($link,$sql)){
+		
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+		if(isset($dados_usuario['usuario'])){
+			$email_existe = true;
+		}
+	} else {
+		echo 'Erro ao tentar localizar o registro de email';
+	}
+
+	if($usuario_existe || $email_existe){
+		if($usuario_existe){
+			$retorno_get.= "erro_usuario=1&";
+		}
+		if($email_existe){
+			$retorno_get.= "erro_email=1&";
+		}		
+		header('Location: inscrevase.php?'.$retorno_get);
+		die();		
+	}
 	$sql = " insert into usuarios(usuario,email,senha) values ('$usuario','$email','$senha') ";
 	//executar a query
 	if(mysqli_query($link, $sql)){
