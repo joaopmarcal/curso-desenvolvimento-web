37f5492a7fb7ea74829341cc9b5177375131fade
João Paulo <paulospc21@gmail.com>
2018-12-02 18:55:42 -0300

    controlando a exibição de botões seguir e deixar de seguir

--- twitter_clone/procurar_pessoas.php
+++ twitter_clone/procurar_pessoas.php
@@ -32,6 +32,10 @@ if(!isset($_SESSION['usuario'])){
 								$('#pessoas').html(data);
 								$('.btn_seguir').click( function(){
 									var id_usuario = $(this).data('id_usuario');
+									
+									$('#btn_seguir_'+id_usuario).hide();
+									$('#btn_deixar_seguir_'+id_usuario).show();
+									
 									$.ajax({
 										url: 'seguir.php',
 										method: 'post',
@@ -44,6 +48,10 @@ if(!isset($_SESSION['usuario'])){
 								
 								$('.btn_deixar_seguir').click( function(){
 									var id_usuario = $(this).data('id_usuario');
+									
+									$('#btn_seguir_'+id_usuario).show();
+									$('#btn_deixar_seguir_'+id_usuario).hide();		
+									
 									$.ajax({
 										url: 'deixar_seguir.php',
 										method: 'post',
